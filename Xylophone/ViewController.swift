74688a298cbc7b7828653620798aab67d29dc382
Vladimir Fibe <motiw@icloud.com>
2022-01-27 09:27:31 +0600

    Закруглил края и задал цвета с теми же именами из Accets

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -46,14 +46,14 @@ class ViewController: UIViewController {
   }
 
   func setupKeys() {
-    let colors = [#colorLiteral(red: 1, green: 0.2332399487, blue: 0.1861645281, alpha: 1), #colorLiteral(red: 0.802870214, green: 0.466054678, blue: 0.02099951915, alpha: 1), #colorLiteral(red: 0.8019409776, green: 0.6408478618, blue: 0.03579719737, alpha: 1), #colorLiteral(red: 0.1716196239, green: 0.6244929433, blue: 0.2817935944, alpha: 1), #colorLiteral(red: 0.2753639221, green: 0.270437181, blue: 0.6706653237, alpha: 1), #colorLiteral(red: 0.02625587583, green: 0.3824955523, blue: 0.7998301387, alpha: 1), #colorLiteral(red: 0.5465310216, green: 0.2587155104, blue: 0.6929754615, alpha: 1)]
     let labels = ["C", "D", "E", "F", "G", "A", "B"]
     for i in 0..<7 {
       let button = UIButton()
       button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 40)
-      button.backgroundColor = colors[i]
+      button.backgroundColor = UIColor.init(named: labels[i])
       button.setTitle(labels[i], for: .normal)
       button.addTarget(self, action: #selector(keyPressed), for: .touchUpInside)
+      button.layer.cornerRadius = 10
       buttons.append(button)
       view.addSubview(button)
     }
